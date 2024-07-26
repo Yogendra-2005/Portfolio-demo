367d14c08ff45dec483a337da34eb864a88d26b5
Yogendra-2005 <singhyogendar669@gmail.com>
2024-07-26 18:17:12 +0530

    ALMOST RESPONSIVE

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
 var typed = new Typed("#element", {
   strings: ["Frontend Developer", "&amp; Software Engineer"],
   typeSpeed: 40,
